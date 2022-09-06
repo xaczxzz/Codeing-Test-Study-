55b05851984e2d55bc199fb563d0f0bcfcaa3709
xaczxzz <101166893+xaczxzz@users.noreply.github.com>
2022-09-06 18:02:21 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int search(int[] arr, int N, int M) {
+        int result = 0;
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int total = arr[i] + arr[j] + arr[k];
+
+                    if (total == M) {
+
+                        return total;
+                    }
+                    if (result < total && total < M) {
+                        result = total;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int [] arr = new int[N];
+        st = new StringTokenizer(br.readLine()," ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = search(arr, N, M);
+        System.out.println(result);
+
+
+    }
+}
+
