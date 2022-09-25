350f794d8edbfb72b18e649e3f536a73daa15335
xaczxzz <101166893+xaczxzz@users.noreply.github.com>
2022-09-25 21:32:45 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 340 ms, Memory: 47840 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Main {
+    static char [][] arr;
+    private static  void star(int x, int y,int N, boolean blank){
+        if(blank){
+            for(int i=x; i<x+N; i++){
+                for(int j=y; j<y+N; j++){
+                    arr[i][j] = ' ';
+                }
+            }
+            return;
+        }
+        if(N==1){
+            arr[x][y] = '*';
+            return;
+        }
+        int size = N / 3;
+        int count = 0;
+        for(int i=x; i<x + N; i+=size) {
+            for(int j=y; j<y + N; j+=size) {
+                count ++;
+
+                if(count == 5) {
+                    star(i, j, size, true);
+                }
+                else {
+                    star(i, j, size, false);
+                }
+            }
+        }
+
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        arr = new char[N][N];
+        star(0,0,N,false);
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                sb.append(arr[i][j]);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
